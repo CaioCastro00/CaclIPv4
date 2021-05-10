f37886e025d5a35c4cdc64c5e874aa979fae05ff
Caio Castro <caiocastro@poli.ufrj.br>
2021-05-10 11:42:42 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 from classes.calcipv4 import CalcIPv4
 
-calc_ipv4 = CalcIPv4(ip="192.168.0.1", mask='255.255.255.192')
+if __name__ == '__main__':
+    
+    calc_ipv4 = CalcIPv4(ip="192.168.0.1", mask='255.255.255.192')
 
-print(f'IP: {calc_ipv4.ip}')
-print(f'Mask: {calc_ipv4.mask}')
-print(f'Net: {calc_ipv4.net}')
-print(f'Broadcast: {calc_ipv4.broadcast}')
-print(f'Prefix: {calc_ipv4.prefix}')
-print(f'Numbers of IPs: {calc_ipv4.num_of_ips}')
+    print(f'IP: {calc_ipv4.ip}')
+    print(f'Mask: {calc_ipv4.mask}')
+    print(f'Net: {calc_ipv4.net}')
+    print(f'Broadcast: {calc_ipv4.broadcast}')
+    print(f'Prefix: {calc_ipv4.prefix}')
+    print(f'Numbers of IPs: {calc_ipv4.num_of_ips}')
